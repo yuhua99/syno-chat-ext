69dfb86aad7eb25ad1d4566180c5c91de3a524a5
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2024-06-30 21:37:07 +0800

    feat: add chatRoomSearch, esc msgbox keyboard shortcut

--- src/common/element.js
+++ src/common/element.js
@@ -1,2 +1,8 @@
 // Global element getter
+
+// chat area
 export let chatMainMsgInputBox = () => document.querySelector(".chat-center-content-panel .msg-inputarea-textarea-wrap>div#ext-comp-1185");
+
+// gadgets
+export let chatRoomSearch = () => document.querySelector('.jump-to-button>button');
+export let chatRoomSearchInputBox = () => document.querySelector(".jump-dialog input");
