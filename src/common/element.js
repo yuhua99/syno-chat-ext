2c8d0a26646f524951298d4b3512ec5b4d9672ac
yuhuahung <yuhuahung@synology.com>
2024-07-01 11:16:18 +0800

    fix: fix chatMainMsgInputBox selector

--- src/common/element.js
+++ src/common/element.js
@@ -1,7 +1,7 @@
 // Global element getter
 
 // chat area
-export let chatMainMsgInputBox = () => document.querySelector(".chat-center-content-panel .msg-inputarea-textarea-wrap>div#ext-comp-1185");
+export let chatMainMsgInputBox = () => document.querySelector(".chat-center-content-panel .chat-input-aria-main-v2 x-border-panel .msg-inputarea-textarea-wrap");
 
 // gadgets
 export let chatRoomSearch = () => document.querySelector('.jump-to-button>button');
