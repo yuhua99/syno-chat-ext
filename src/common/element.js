0e01a3df728d095d5462e87de6a8836b36c667fb
Elven Lin, RetrO9 <elvenlin1998@gmail.com>
2024-07-01 14:56:04 +0800

    fix: fix chatMainMsgInputBox selector

--- src/common/element.js
+++ src/common/element.js
@@ -1,7 +1,7 @@
 // Global element getter
 
 // chat area
-export let chatMainMsgInputBox = () => document.querySelector(".chat-center-content-panel .chat-input-aria-main-v2 x-border-panel .msg-inputarea-textarea-wrap");
+export let chatMainMsgInputBox = () => document.querySelector(".chat-center-content-panel .chat-input-aria-main-v2 .msg-inputarea-textarea-wrap .msg-inputarea-textarea");
 
 // gadgets
 export let chatRoomSearch = () => document.querySelector('.jump-to-button>button');
