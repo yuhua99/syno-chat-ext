1fd5febea84ef684200758c052f9ad96e41e7bf5
yuhuahung <yuhuahung@synology.com>
2024-07-10 16:51:19 +0800

    feat: refactor color remap logic

--- src/content_scripts/index.ts
+++ src/content_scripts/index.ts
@@ -1,13 +1,18 @@
 // --------------- [ SCSS Style ] ---------------
-import "./styles/content_scripts.scss";
+import "./styles/content_scripts.scss"
 
 // --------------- [ TS Module ] ----------------
-import "./scripts/image_button.ts";
-import "./scripts/keyboard_shortcut.ts";
-import { setBackgroundColor } from "./scripts/style";
+import "./scripts/image_button"
+import "./scripts/keyboard_shortcut"
+import { bgColor, setBackgroundColor } from "./scripts/style"
+import { colorToHex } from "./scripts/color"
 
+function setProperty(name: string, value: string) {
+    document.documentElement.style.setProperty('--' + name, value)
+    console.log(name, value)
+}
 
 window.addEventListener("load", async function () {
-    console.info("Content script loaded");
-    setBackgroundColor();
-});
+    setProperty('background-color', colorToHex(bgColor))
+    setBackgroundColor()
+})
--- src/content_scripts/scripts/style.ts
+++ src/content_scripts/scripts/style.ts
@@ -1,83 +1,57 @@
-function padZero(str: string): string {
-    return str.length === 1 ? '0' + str : str;
+import {
+    Color,
+    adjustBrightness,
+    colorToRgb,
+    toColor
+} from "./color"
+
+type CSSProperty = keyof CSSStyleDeclaration
+
+export const bgColor = toColor("#424242")
+export const bgColorDark = adjustBrightness(bgColor, -30)
+export const bgColorLight = adjustBrightness(bgColor, 30)
+export const primaryColor = toColor("#f1beb0")
+
+// TODO: fix sidebar color remap notworking
+
+const colorMap: { [key: string]: Color } = {
+    "rgb(255, 255, 255)": bgColor,
+    "rgb(245, 249, 250)": bgColorLight,
+    "rgb(50, 61, 90)": bgColorDark, // sidebar
+    "rgb(87, 96, 120)": bgColor, // sidebar hover
+    "rgb(99, 189, 112)": primaryColor,
+    "rgb(95, 184, 109)": primaryColor,
 }
 
-function hexToRgb(hex: string): { r: number, g: number, b: number } {
-    if (hex.startsWith('#')) {
-        hex = hex.slice(1);
-    }
-    if (hex.length === 3) {
-        hex = hex.split('').map(x => x + x).join('');
-    }
-    return {
-        r: parseInt(hex.slice(0, 2), 16),
-        g: parseInt(hex.slice(2, 4), 16),
-        b: parseInt(hex.slice(4, 6), 16)
-    };
+function replaceColor(color: string): string {
+    const mappedColor = colorMap[color]
+
+    return mappedColor ? colorToRgb(mappedColor) : color
 }
 
-function rgbToHex(r: number, g: number, b: number): string {
-    return '#' + [r, g, b].map(x => padZero(x.toString(16))).join('');
+function isColorProperty(value: string): boolean {
+    return (value.startsWith('rgb') || value.startsWith('#'))
 }
 
-// Convert RGBA color to hex
-function rgbaColorToHex(rgb: string): string {
-    return '#' + rgb.match(/\d+/g)!.map(x => parseInt(x).toString(16).padStart(2, '0')).join('');
+function replaceColorPropertiesOfRule(rule: CSSStyleRule): void {
+    const properties: string[] = ['backgroundColor', 'background', 'color', 'borderColor']
+    const style: CSSStyleDeclaration = rule.style
+    properties.forEach(property => {
+        const value = style[property as CSSProperty]
+        if (typeof value === 'string' && isColorProperty(value)) {
+            style.setProperty(property, replaceColor(value))
+        }
+    })
 }
 
-function calculateBrightness(r: number, g: number, b: number): number {
-    return (r * 299 + g * 587 + b * 114) / 1000;
-}
+function processStyleSheet(styleSheet: CSSStyleSheet): void {
+    if (!styleSheet.cssRules) return
 
-function calculateSaturation(r: number, g: number, b: number): number {
-    let max = Math.max(r, g, b);
-    let min = Math.min(r, g, b);
-    return max - min;
-}
-
-function adjustBrightness(r: number, g: number, b: number, brightness: number): { r: number, g: number, b: number } {
-    if ([r, g, b, brightness].some(arg => typeof arg !== 'number')) {
-        throw new Error('All arguments must be numbers.');
-    }
-    return {
-        r: Math.max(0, Math.min(255, Math.floor(r + brightness))),
-        g: Math.max(0, Math.min(255, Math.floor(g + brightness))),
-        b: Math.max(0, Math.min(255, Math.floor(b + brightness))),
-    };
-}
-
-function adjustColor(hex: string): string {
-    let { r, g, b } = hexToRgb(hex);
-    let brightness = calculateBrightness(r, g, b);
-    let saturation = calculateSaturation(r, g, b);
-    let percent = saturation < 32 ? 128 : 15;
-    let { r: adjustedR, g: adjustedG, b: adjustedB } = adjustBrightness(r, g, b, percent * (brightness < 128 ? 1 : -1));
-    return rgbToHex(adjustedR, adjustedG, adjustedB);
+    Array.from(styleSheet.cssRules)
+        .filter(rule => rule instanceof CSSStyleRule)
+        .forEach(rule => replaceColorPropertiesOfRule(rule as CSSStyleRule))
 }
 
 export function setBackgroundColor(): void {
-  // Iterate over all stylesheets
-  for (let i = 0; i < document.styleSheets.length; i++) {
-    let styleSheet = document.styleSheets[i];
-    try {
-      // Not all stylesheets have a cssRules property
-      if (styleSheet.cssRules) {
-        // Iterate over all rules in the stylesheet
-        for (let j = 0; j < styleSheet.cssRules.length; j++) {
-          let rule = styleSheet.cssRules[j];
-          // Check if the rule is a CSSStyleRule and has a backgroundColor property
-          if (rule instanceof CSSStyleRule && rule.style.backgroundColor) {
-            // Set the backgroundColor to green
-            let origColor = rule.style.backgroundColor;
-            if (origColor.startsWith('rgb')) {
-              rule.style.backgroundColor = adjustColor(rgbaColorToHex(origColor));
-            }
-          }
-        }
-      }
-    } catch (e) {
-      console.error(`Could not access rules for stylesheet at ${styleSheet.href}: ${e}`);
-    }
-  }
+    Array.from(document.styleSheets).forEach(processStyleSheet)
 }
-
