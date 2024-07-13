4c6c0cba1131bafddb6f570f1e4846ceebb25b29
isbecky27 <is.becky27@gmail.com>
2024-07-13 18:19:59 +0800

    fix: the color settings of the popup

--- src/common/storage.ts
+++ src/common/storage.ts
@@ -1,10 +1,12 @@
-export const getStorage = async (
-    componentKey: string
-) => {
-    let data = await chrome.storage.sync.get(componentKey)
-    return data[componentKey]
+export const getStorage = async (componentKey: string) => {
+    const value = await chrome.storage.sync.get(componentKey).then((result) => {
+        return result[componentKey];
+    });
+    return value;
 };
 
 export const setStorage = async (componentKey: string, value: any) => {
-    await chrome.storage.sync.set({ componentKey: value });
+    await chrome.storage.sync.set({ [componentKey]: value }).then(() => {
+        console.log(componentKey, "is set to", value);
+    });
 };
