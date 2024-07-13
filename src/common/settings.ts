92ff48b330587207fcb1ac7a2d87ba232039de70
isbecky27 <is.becky27@gmail.com>
2024-07-13 18:19:59 +0800

    fix: the color settings of the popup

--- src/common/settings.ts
+++ src/common/settings.ts
@@ -2,5 +2,5 @@
 export const CHROME_SYNC_STORAGE_KEY = "syno-chat-extension";
 
 // ------------ [CSS Settings] ------------
-export const PRIMARY_COLOR = "primary-color"
-export const BG_COLOR = "background-color"
+export const PRIMARY_COLOR = "primary-color";
+export const BG_COLOR = "background-color";
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -8,13 +8,22 @@ import {
     BG_COLOR,
 } from "common/settings"
 
-window.onload = function () {
-    const primaryColorInput = document.getElementById('primaryColor') as HTMLInputElement
-    const bgColorInput = document.getElementById('backgroundColor') as HTMLInputElement
-    const saveButton = document.getElementById('save')
+document.addEventListener('DOMContentLoaded', async function() {
+    const primaryColorInput = document.getElementById('primaryColor') as HTMLInputElement;
+    const bgColorInput = document.getElementById('backgroundColor') as HTMLInputElement;
+    const saveButton = document.getElementById('save');
 
-    saveButton.addEventListener('click', function () {
-        setStorage(BG_COLOR, bgColorInput.value)
-        setStorage(PRIMARY_COLOR, primaryColorInput.value)
-    })
-};
+    const primaryColorStored = await getStorage(PRIMARY_COLOR);
+    if (primaryColorStored) {
+        primaryColorInput.value = primaryColorStored;
+    }
+    const bgColorStored = await getStorage(BG_COLOR);
+    if (bgColorStored) {
+        bgColorInput.value = bgColorStored;
+    }
+
+    saveButton.addEventListener('click', async function () {
+        await setStorage(PRIMARY_COLOR, primaryColorInput.value);
+        await setStorage(BG_COLOR, bgColorInput.value);
+    });
+});
